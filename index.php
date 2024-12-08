b336c5519bba749ca542dd84699c2f0dadcf7b88
erwinsantander <santandererwin4@gmail.com>
2024-12-08 00:29:25 -0800

    commit

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
     if ($session->isUserLoggedIn()) {
         redirect('admin.php', false);
     }
+
+
+    header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Permissions-Policy: geolocation=(self), microphone=()");
 ?>
 
 <!DOCTYPE html>
